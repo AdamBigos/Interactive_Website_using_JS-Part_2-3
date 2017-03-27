f56925b03cb124fe06f7a65a58ea0f562cbe678a
AdamBigos <AdamBigos@users.noreply.github.com>
2017-03-27 17:41:28 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 var main = function(){
-    $('.dropdown-toggle').click(function(){
-    $('.weather_forcast').toggle();
-    });
     
-    $('.arrow-next').click(function(){
+    var menuDropdown = function(){
+    $('.weather_forcast').toggle();
+    }
+    
+    $('.dropdown-toggle').click(menuDropdown);
+    
+    var sliderEventEfect = function(){
+        
+        if(this.class == "arrow-next"){
         
         var currentSlide = $('.active-slide');
         var nextSlide = currentSlide.next();
@@ -24,9 +29,69 @@ var main = function(){
         
         currentDot.removeClass('active-dot');
         nextDot.addClass('active-dot');
-    });
-    
-    $('.arrow-prev').click(function(){
+            
+        } else if(this.class == 'd1'){
+            $('.robot').removeClass('active-slide');
+            $('.r1').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d1').addClass('active-dot');
+            
+            
+        } else if(this.class == 'd2'){
+            $('.robot').removeClass('active-slide')
+            $('.r2').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d2').addClass('active-dot');
+            
+        } else if(this.class == 'd3'){
+            $('.robot').removeClass('active-slide');
+            $('.r3').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d3').addClass('active-dot');
+            
+        } else if(this.class == 'd4'){
+            $('.robot').removeClass('active-slide');
+            $('.r4').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d4').addClass('active-dot');
+            
+        } else if(this.class == 'd5'){
+            $('.robot').removeClass('active-slide');
+            $('.r5').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d5').addClass('active-dot');
+            
+        } else if(this.class == 'd6'){
+            $('.robot').removeClass('active-slide');
+            $('.r6').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d6').addClass('active-dot');
+            
+        } else if(this.class == 'd7'){
+            $('.robot').removeClass('active-slide');
+            $('.r7').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d7').addClass('active-dot');
+            
+        } else if(this.class == 'd8'){
+            $('.robot').removeClass('active-slide');
+            $('.r8').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d8').addClass('active-dot');
+            
+        } else if(this.class == 'd9'){
+            $('.robot').removeClass('active-slide');
+            $('.r9').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d9').addClass('active-dot');
+            
+        } else if(this.class == 'd10'){
+            $('.robot').removeClass('active-slide');
+            $('.r10').addClass('active-slide');
+            $('.dot').removeClass('active-dot');
+            $('.d10').addClass('active-dot');
+            
+        } else if(this.class == 'arrow-prev'){
         
         var currentSlide = $('.active-slide');
         var nextSlide = currentSlide.prev();
@@ -45,18 +110,23 @@ var main = function(){
         }
         currentDot.removeClass('active-dot');
         nextDot.addClass('active-dot');
-    });
+        }
+    }
     
-    $('.button').click(function() {
+    $('.slide').click(sliderEventEfect)
+    
+    var buttonEvent = function() {
         var post = $('.form').val();
 
         $('<li>').text(post).prependTo('.posts');
         $('.form').val('');
         $('.counter').text('200');
         $('.button').addClass('disabled'); 
-    });
-  
-    $('.form').keyup(function() {
+    }
+    
+    $('.button').click(buttonEvent);
+    
+    var formEvent = function() {
         var postLength = $(this).val().length;
         var charactersLeft = 200 - postLength;
         $('.counter').text(charactersLeft);
@@ -70,19 +140,12 @@ var main = function(){
         else {
           $('.button').removeClass('disabled');
         }
-    });
+    }
+    
+  
+    $('.form').keyup(formEvent);
   
     $('.button').addClass('disabled');
-    
-    $('robota h1').click(function() {
-        setInterval(function(){ 
-            $('.robot').fadeOut(10)
-            .next()
-            .fadeIn(10)
-            .end()
-            .appendTo('.robot'); 
-        }, 1000);
-    });
     
 };
 
